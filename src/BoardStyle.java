12705b89b589f15e3ac2b1c9182730c7b5dc03e1
Bashir535 <168224922+Bashir535@users.noreply.github.com>
2026-04-28 17:35:38 -0700

    Refactor drawPit and drawMancala method signatures

--- src/BoardStyle.java
+++ src/BoardStyle.java
@@ -23,10 +23,7 @@ public interface BoardStyle {
 
     Font getMancalaFont();
 
-    void drawPit(Graphics2D g2, int x, int y, int w, int h,
-                 int stones, String label,
-                 boolean isCurrentPlayerPit, boolean isHovered);
+    void drawPit(Graphics2D g2, int x, int y, int w, int h, int stones, String label, boolean isCurrentPlayerPit, boolean isHovered);
 
-    void drawMancala(Graphics2D g2, int x, int y, int w, int h,
-                     int stones, String label);
+    void drawMancala(Graphics2D g2, int x, int y, int w, int h, int stones, String label);
 }
